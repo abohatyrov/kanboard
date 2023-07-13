5bd8eb3ad2da9eda4e9dc4bec8b4bcb72b97dd1e
abohatyrov <bohatyrovandrey@gmail.com>
2023-07-13 15:26:10 +0300

    Change config for tests

--- tests/configs/config.mysql.php
+++ tests/configs/config.mysql.php
@@ -1,10 +1,10 @@
 <?php
 
 define('DB_DRIVER', 'mysql');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'kanboard');
 define('DB_PASSWORD', 'kanboard');
-define('DB_HOSTNAME', 'mysql');
-define('DB_NAME', 'kanboard');
+define('DB_HOSTNAME', '10.26.0.39');
+define('DB_NAME', 'kanboard_test');
 
 define('DEBUG', true);
 define('LOG_DRIVER', 'stderr');
